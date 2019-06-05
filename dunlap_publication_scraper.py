6e99bb2e83703fe077fff044bd6e40a58d8d5afc
Bryan Gaensler <bgaensler@dunlap.utoronto.ca>
2019-06-05 16:55:28 -0400

    Rewritten to directly use paper affiliations rather than Dunlap personnel database

--- dunlap_publication_scraper.py
+++ dunlap_publication_scraper.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
+
 # Code to automatically scrape Dunlap Directory and ADS, and identify recent Dunlap publications and highlight authors.
 # Written by Bryan Gaensler for Dunlap Hack Day, 4 June 2019
 
 # Get 5 most recent Dunlap papers from ADS
-# Uses https://ads.readthedocs.io/en/latest/
 
 import ads
 import datetime
@@ -12,54 +13,30 @@ import requests
 
 # Number of papers to return
 num = 5
-adsname = range(num)
-adsname2 = range(num)
-bib = range(num)
-title = range(num)
-pdate = range(num)
-authstr = range(num)
-finalstr = range(num)
 
 # Gaensler ADS token
 ads.config.token = ''
-papers = ads.SearchQuery(q="aff:Dunlap Institute", sort="date", rows=num)
-for i,paper in enumerate(papers):
-    # Now shorten each author to ADS format
-    adsname[i] = range(len(paper.author))
-    adsname2[i] = range(len(paper.author))
-    bib[i] = paper.bibcode.encode('ascii')
-    title[i] = unidecode.unidecode(paper.title[0])
-    pdate[i] = datetime.datetime.strptime(paper.pubdate[0:7], "%Y-%m").strftime('%b %Y')
-    for n in range(len(paper.author)):
-        # Where is comma?
-        m = paper.author[n].find(',')
-        adsname[i][n] = unidecode.unidecode(paper.author[n][0:m+3]+'.')    
-        adsname2[i][n] = adsname[i][n]
 
-# Now scrape Dunlap personnel page for list of possible authors
-# http://www.dunlap.utoronto.ca/people/directory/
+# Query ADS
+# Uses https://ads.readthedocs.io/en/latest/
+papers = list(ads.SearchQuery(q="aff:Dunlap Institute", sort="date", rows=num))
 
-directory = "http://www.dunlap.utoronto.ca/people/directory/"
+authstr = ['' for x in range(num)]
+pdate = ['' for x in range(num)]
+title = ['' for x in range(num)]
+bib = ['' for x in range(num)]
+finalstr = ['' for x in range(num)]
 
-page = requests.get(directory)
-tree = html.fromstring(page.content)
-name1 = tree.xpath('//span[@class="given-name"]/text()')
-name2 = tree.xpath('//span[@class="family-name"]/text()')
-
-# Now form author strings formatted for ads
-dunlapname = ["" for x in range(len(name1))]
-for n in range(0,len(name1)):
-    dunlapname[n] = unidecode.unidecode(name2[n])+', '+unidecode.unidecode(name1[n][0])+'.'
-
-
-# Now find the Dunlap authors in each ADS paper and make them bold
-
-for n in range(num):
-    for name in dunlapname:
-        if (name in adsname[n]):
-            i = adsname[n].index(name)
-            adsname2[n][i] = "<strong>"+name+"</strong>"
-
+for n,paper in enumerate(papers):
+    for i in range(len(paper.author)):
+        # Make Dunlap authors in bold
+        if 'Dunlap Institute' in paper.aff[i]:
+            paper.author[i] = "<strong>"+paper.author[i]+"</strong>"
+    authstr[n] = ", ".join(paper.author)
+    pdate[n] = datetime.datetime.strptime(paper.pubdate[0:7], "%Y-%m").strftime('%b %Y')
+    title[n] = unidecode.unidecode(paper.title[0])
+    bib[n] = paper.bibcode
+    finalstr[n] = '<a href="https://ui.adsabs.harvard.edu/abs/'+bib[n]+'" target="_blank">'+authstr[n]+', '+title[n]+', '+pdate[n]+'</a> <p>'
 
 # Now write output to WWW
 
@@ -67,13 +44,11 @@ f = open("/Users/bmg/Dropbox/dunlap_publications.html","w")
 
 f.write('<h3>Recent Dunlap Publications</h3>'+'\n')
 for n in range(num):
-    authstr[n] = adsname2[n][0]
-    for m in range(1,len(adsname2[n])):
-        authstr[n] = authstr[n]+'; '+adsname2[n][m]
-        finalstr[n] = '<a href="https://ui.adsabs.harvard.edu/abs/'+bib[n]+'" target="_blank">'+authstr[n]+', '+title[n]+', '+pdate[n]+'</a> <p>'
-#    finalstr[n] = authstr[n]+', '+pdate[n]
-    f.write(str(n+1)+'. '+finalstr[n]+'\n')
+     f.write(str(n+1)+'. '+finalstr[n]+'\n')
 
 f.write('<p><a href="https://ui.adsabs.harvard.edu/#search/q=aff%3A%22Dunlap%20Institute%22&sort=date%20desc" target="_blank">Complete list >></a></p>')
 f.close()
+
+
+
 
