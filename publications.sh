b0ea72acba175f86354355af2a078d0fb7cd48af
Bryan Gaensler <bgaensler@dunlap.utoronto.ca>
2019-06-20 09:26:39 -0400

    Delete publications.sh

--- publications.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/csh -f
-wget --no-cache --secure-protocol=auto -r https://www.dropbox.com/s/8hsswke8csv7q37/publication_list.html -O /web/httpd/users/bgaensler/public_html/dunlap_publications.html >& /dev/null
-chmod 644 /web/httpd/users/bgaensler/public_html/dunlap_publications.html
